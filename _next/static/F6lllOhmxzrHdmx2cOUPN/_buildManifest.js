cadf69b7388191388d70ecd019fe057d802db631
flavioislima <flavioislima@users.noreply.github.com>
2022-03-22 19:44:01 +0000

    deploy: fcbb3f08f7c99a63ef00522c57d40109caedd2ff

---
+++ _next/static/F6lllOhmxzrHdmx2cOUPN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e67638eeb466b093.css","static/chunks/pages/index-da915bd3022f08a5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
